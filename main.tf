b618c3fb23973f88a5496a0a3ab2cfd2ee3fc3db
Tonyb23 <ubanianthony23@gmail.com>
2024-05-19 20:19:57 +0100

    initial terraform configuration

---
+++ main.tf
@@ -0,0 +1,23 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "3.103.1"
+    }
+  }
+}
+
+locals {
+  tags = {
+    Environment = var.environment // Ensure variable name consistency
+  }
+}
+resource "azurerm_storage_account" "securestorage" {
+  resource_group_name           = var.resource_group_name
+  location                      = var.location
+  name                          = var.storage_account_name
+  account_tier                  = "Standard"
+  account_replication_type      = var.environment == "Production" ? "GRS" : "LRS"
+  public_network_access_enabled = false
+  tags                          = local.tags
+}
---
+++ variables.tf
@@ -0,0 +1,16 @@
+variable "environment" {
+  description = "The environment either production or development"
+  type        = string
+}
+variable "resource_group_name" {
+  description = "The name of the resource group."
+  type        = string
+}
+variable "location" {
+  description = "The location where resources will be created."
+  type        = string
+}
+variable "storage_account_name" {
+  description = "The name of the storage account."
+  type        = string
+}
